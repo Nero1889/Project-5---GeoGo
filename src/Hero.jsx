f62215057f10cc294650d5167ce1ee80845e8b8e
Raul Luna <mceclipse1889@gmail.com>
2025-06-03 17:06:18 -0500

    Update Hero.jsx

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,6 +1,30 @@
 function Hero() {
     return (
         <div className="hero">
+            <div id="hero-title">Embark on Your Next Journey Today!</div>
+            <p id="hero-desc">Explore our stunning gallery of pictures and opportunities
+            for you! Join us on your next adventure on a corner of our beautiful planet
+            Earth.</p>
+            <button className="btn">Contact Us!</button>
+
+            <div id="a">
+
+                <div className="b">
+                    <h1 className="card-title"></h1>
+                    <p id="card-desc"></p>
+                </div>
+
+                <div className="b">
+                    <h1 className="card-title"></h1>
+                    <p id="card-desc"></p>
+                </div>
+
+                <div className="b">
+                    <h1 className="card-title"></h1>
+                    <p id="card-desc"></p>
+                </div>
+                
+            </div>
         </div>
     )
 }

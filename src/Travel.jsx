0c43bce7e638ee7229a86b16f2aff6bee9ffde7e
Raul Luna <mceclipse1889@gmail.com>
2025-06-04 17:21:49 -0500

    Add files via upload

---
+++ src/Travel.jsx
@@ -0,0 +1,9 @@
+function Travel() {
+    return (
+        <div className="travel">
+            <h1 id="travel-title">Travel Successfully Deployed!</h1>
+        </div>
+    )
+}
+
+export default Travel;

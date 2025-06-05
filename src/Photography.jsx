dd2db2ac95a54876b893b13858d53accf9ebe37f
Raul Luna <mceclipse1889@gmail.com>
2025-06-05 18:02:00 -0500

    Add files via upload

---
+++ src/Photography.jsx
@@ -0,0 +1,17 @@
+function Photography() {
+    return (
+        <div className="photography">
+            <h1 id="photo-title">Capturing The Beauty of Our Planet</h1>
+            <p id="photo-desc">View the many beautiful pictures taken by our guests
+            and photographers!</p>
+
+            <div id="photo-container">
+                <div className="photo-preview"></div>
+                <div className="photo-preview"></div>
+                <div className="photo-preview"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Photography;

55cdc90330d6d18f2b62644a301cdd40e8052de6
Raul Luna <mceclipse1889@gmail.com>
2025-06-05 17:40:11 -0500

    Update Travel.jsx

--- src/Travel.jsx
+++ src/Travel.jsx
@@ -1,7 +1,33 @@
+import california from "./assets/losAngeles.png";
+import japan from "./assets/japan.png";
+import spain from "./assets/madrid.jpg";
+
 function Travel() {
     return (
         <div className="travel">
-            <h1 id="travel-title">Travel Successfully Deployed!</h1>
+            <div id="travel-container">
+
+                <div className="travel-card">
+                    <img className="travel-img" src={california} alt="California"/>
+                    <p className="travel-title">California</p>
+                </div>
+
+                <div className="travel-card">
+                    <img className="travel-img" src={japan} alt="Japan"/>
+                    <p className="travel-title">Japan</p>
+                </div>
+
+                <div className="travel-card">
+                    <img className="travel-img" src={spain} alt="Spain"/>
+                    <p className="travel-title">Spain</p>
+                </div>
+                <p id="more-txt">and more...</p>
+            </div>
+
+            <div id="with-container">
+                <h1 id="with-title">Travel With Us!</h1>
+                <p id="with-desc">Thinking of a destination you’d like to make memories in? If you’d like to explore the world then let us help you!</p>
+            </div>
         </div>
     )
 }

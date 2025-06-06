e9fdd3b4308e5fb29cc525e7707f598261e46d54
Raul Luna <mceclipse1889@gmail.com>
2025-06-06 16:42:18 -0500

    Add files via upload

---
+++ src/Questions.jsx
@@ -0,0 +1,10 @@
+function Questions() {
+    return (
+        <div className="questions">
+            <p id="questions-desc">Got any questions?</p>
+            <h1 id="questions-title">Frequently Asked Questions</h1>
+        </div>
+    )
+}
+
+export default Questions;

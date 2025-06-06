7a837e687075506397976fbdfe7b1338d5931106
Raul Luna <mceclipse1889@gmail.com>
2025-06-06 16:59:57 -0500

    Update Questions.jsx

--- src/Questions.jsx
+++ src/Questions.jsx
@@ -1,8 +1,58 @@
 function Questions() {
     return (
         <div className="questions">
-            <p id="questions-desc">Got any questions?</p>
+            <p id="questions-sub-title">Got any questions?</p>
             <h1 id="questions-title">Frequently Asked Questions</h1>
+            <div id="questions-container">
+
+                <div className="question">
+                    <div className="sub-ques-container">
+                        <p className="expand">+</p>
+                        <p className="question-desc">Why travel with us?</p>
+                    </div>
+                </div>
+
+                <div className="question-bar"></div>
+
+                <div className="question">
+                    <div className="sub-ques-container">
+                        <p className="expand">+</p>
+                        <p className="question-desc">Upon choosing a destination... How
+                        long can we stay to explore??</p>
+                    </div>
+                </div>
+
+                <div className="question-bar"></div>
+
+                <div className="question">
+                    <div className="sub-ques-container">
+                        <p className="expand">+</p>
+                        <p className="question-desc">What amenities are provided on 
+                        arrival?</p>
+                    </div>
+                </div>
+
+                <div className="question-bar"></div>
+
+                <div className="question">
+                    <div className="sub-ques-container">
+                        <p className="expand">+</p>
+                        <p className="question-desc">Can we choose to visit a country 
+                        then drive, bike or travel to another?</p>
+                    </div>
+                </div>
+
+                <div className="question-bar"></div>
+
+                <div className="question">
+                    <div className="sub-ques-container">
+                        <p className="expand">+</p>
+                        <p className="question-desc">What are additional services 
+                        provided throughout the visit?</p>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }

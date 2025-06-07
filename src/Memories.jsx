8b02512e25d2eef24849f958658f31a7853ff505
Raul Luna <mceclipse1889@gmail.com>
2025-06-06 19:15:18 -0500

    Add files via upload

---
+++ src/Memories.jsx
@@ -0,0 +1,12 @@
+function Memories() {
+    return (
+        <div className="memories">
+            <h3 id="memories-sub-title">Your Safety is Our #1 Priority</h3>
+            <h1 id="memories-title">Make Memories</h1>
+            <p id="memories-desc">If you've had a vacation you've had in your head or
+            planned out for a while now, we can help you make it a reality!</p>
+        </div>
+    )
+}
+
+export default Memories;

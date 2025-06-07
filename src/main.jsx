1a2ade60ef5cd20da0e436951ea1d8536116bf8a
Raul Luna <mceclipse1889@gmail.com>
2025-06-06 19:15:29 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,14 @@ import "./travel.css";
 import "./photography.css"
 import "./questions.css";
 import "./guests.css";
+import "./memories.css";
 import Header from "./Header.jsx";
 import Hero from "./Hero.jsx";
 import Travel from "./Travel.jsx";
 import Photography from "./Photography.jsx";
 import Questions from "./Questions.jsx";
 import Guests from "./Guests.jsx";
+import Memories from "./Memories.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
@@ -22,5 +24,6 @@ createRoot(document.getElementById("root")).render(
         <Photography/>
         <Questions/>
         <Guests/>
+        <Memories/>
     </StrictMode>,
-)
+);

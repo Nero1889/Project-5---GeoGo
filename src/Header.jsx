7a186b4875c0c83e248a6538e195222070b05c3e
Raul Luna <mceclipse1889@gmail.com>
2025-06-09 18:07:10 -0500

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -10,19 +10,19 @@ function Header() {
     const [IS_MENU_OPEN, setIsMenuOpen] = useState(false);
     const [DARK_MODE, setDarkMode] = useState(() => {
         const STORED_THEME = localStorage.getItem("theme");
-        return STORED_THEME === "light"; 
+        return STORED_THEME === "light";
     });
 
     const TOGGLE_MENU = () => setIsMenuOpen(!IS_MENU_OPEN);
-    const TOGGLE_THEME = () => setDarkMode(prevMode => !prevMode); 
-    
+    const TOGGLE_THEME = () => setDarkMode(prevMode => !prevMode);
+
     useEffect(() => {
         if (DARK_MODE) {
             document.body.classList.add("dark-mode");
-            localStorage.setItem("theme", "light"); 
+            localStorage.setItem("theme", "light");
         } else {
             document.body.classList.remove("dark-mode");
-            localStorage.setItem("theme", "dark"); 
+            localStorage.setItem("theme", "dark");
         }
     }, [DARK_MODE]);
 
@@ -36,15 +36,6 @@ function Header() {
 
     return (
         <>
-            {/* <div className="orb-container">
-                <div className="orb red"></div>
-                <div className="orb orange"></div>
-                <div className="orb yellow"></div>
-                <div className="orb green"></div>
-                <div className="orb blue"></div>
-                <div className="orb purple"></div>
-            </div> */}
-
             <header className="header">
                 <div id="navbar-container">
                     <div id="logo-container">
@@ -70,12 +61,13 @@ function Header() {
             <div className={`mobile-menu ${IS_MENU_OPEN ? "open" : ""}`}>
                 <nav>
                     <ul>
-                        <li><a href="#home" onClick={() => alert("You clicked on: Home!")}>Home</a></li>
-                        <li><a href="#gallery" onClick={() => alert("You clicked on: Gallery!")}>Gallery</a></li>
-                        <li><a href="#faq" onClick={() => alert("You clicked on: FaQ!")}>FaQ</a></li>
-                        <li><a href="#misc" onClick={() => alert("You clicked on: Misc!")}>Misc</a></li>
+                        <li onClick={TOGGLE_MENU}><a href="#home">Home</a></li>
+                        <li onClick={TOGGLE_MENU}><a href="#gallery">Gallery</a></li>
+                        <li onClick={TOGGLE_MENU}><a href="#faq">FaQ</a></li>
+                        <li onClick={TOGGLE_MENU}><a href="#misc">Misc</a></li>
                         <div id="mobile-menu-bar"></div>
-                        <img className="theme-sun" src={sun} alt="Toggle Theme Sun" onClick={TOGGLE_THEME}/>
+                        <img className="theme-sun" src={sun} alt="Toggle Theme Sun" 
+                        onClick={TOGGLE_THEME}/>
                     </ul>
                 </nav>
             </div>

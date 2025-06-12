9e7898c4ba1120bfc5d37275b24352f34a48875a
Raul Luna <mceclipse1889@gmail.com>
2025-06-11 20:05:18 -0500

    Update Travel.jsx

--- src/Travel.jsx
+++ src/Travel.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import california from "./assets/losAngeles.png";
 import japan from "./assets/japan.png";
 import spain from "./assets/madrid.jpg";
@@ -5,74 +6,66 @@ import morocco from "./assets/Morocco.png";
 import brazil from "./assets/Brazil.png";
 import thailand from "./assets/thailand.png";
 
+const DESTINATIONS = [
+    {name: "California", image: california},
+    {name: "Japan", image: japan},
+    {name: "Spain", image: spain},
+    {name: "Morocco", image: morocco},
+    {name: "Brazil", image: brazil},
+    {name: "Thailand", image: thailand},
+];
+
+function TravelCard({image, name}) {
+    return (
+        <div className="travel-card">
+            <img className="travel-img" src={image} alt={name}/>
+            <p className="travel-title">{name}</p>
+        </div>
+    );
+}
+
+function BentoBox({image, name, boxClass}) {
+    return (
+        <div className={`bento-box ${boxClass}`}>
+            <img className="travel-img" src={image} alt={name}/>
+            <p className="bento-travel-title">{name}</p>
+        </div>
+    );
+}
+
 function Travel() {
     return (
         <div className="travel">
             <div id="travel-container">
-                <div className="travel-card">
-                    <img className="travel-img" src={california} alt="California"/>
-                    <p className="travel-title">California</p>
-                </div>
-                <div className="travel-card">
-                    <img className="travel-img" src={japan} alt="Japan"/>
-                    <p className="travel-title">Japan</p>
-                </div>
-                <div className="travel-card">
-                    <img className="travel-img" src={spain} alt="Spain"/>
-                    <p className="travel-title">Spain</p>
-                </div>
-                <div className="travel-card">
-                    <img className="travel-img" src={morocco} alt="Morocco"/>
-                    <p className="travel-title">Morocco</p>
-                </div>
-                <div className="travel-card">
-                    <img className="travel-img" src={brazil} alt="Brazil"/>
-                    <p className="travel-title">Brazil</p>
-                </div>
+                {DESTINATIONS.slice(0, 5).map((dest) => (
+                    <TravelCard key={dest.name} image={dest.image} name={dest.name}/>
+                ))}
                 <div className="bento-grid">
-                    <div className="bento-box box1">
-                        <img className="travel-img" src={brazil} alt="Brazil"/>
-                        <p className="bento-travel-title">Brazil</p>
-                    </div>
-                    <div className="bento-box box2">
-                        <img className="travel-img" src={thailand} alt="Thailand"/>
-                        <p className="bento-travel-title">Thailand</p>
-                    </div>
-                    <div className="bento-box box3">
-                        <img className="travel-img" src={california} alt="California"/>
-                        <p className="bento-travel-title">California</p>
-                    </div>
-                    <div className="bento-box box4">
-                        <img className="travel-img" src={japan} alt="Japan"/>
-                        <p className="bento-travel-title">Japan</p>
-                    </div>
-                    <div className="bento-box box5">
-                        <img className="travel-img" src={spain} alt="Spain"/>
-                        <p className="bento-travel-title">Spain</p>
-                    </div>
-                    <div className="bento-box box6">
-                        <img className="travel-img" src={morocco} alt="Morocco"/>
-                        <p className="bento-travel-title">Morocco</p>
-                    </div>
+                    {DESTINATIONS.map((dest, index) => (
+                        <BentoBox key={`bento-${dest.name}`} image={dest.image}
+                        name={dest.name} boxClass={`box${index + 1}`}/>
+                    ))}
                 </div>
                 <p id="more-txt">and more...</p>
             </div>
             <div id="travel-desc-container">
                 <div id="with-container">
                     <h1 id="with-title">Travel With Us!</h1>
-                    <p id="with-desc">Thinking of a destination you'd like to make memories
-                    in? If you'd like to explore the world then let us help you!</p>
+                    <p id="with-desc">Thinking of a destination you'd like to make 
+                    memories in? If you'd like to explore the world then let us help
+                    you!</p>
                 </div>
                 <div id="sub-travel-desc-container">
                     <div className="sub-with-container">
                         <h1 className="sub-with-title">Starting Your Journey</h1>
-                        <p className="sub-with-desc">Let us know about your desired journey, and
-                        leave the rest to us! We'll make your adventure a reality!</p>
+                        <p className="sub-with-desc">Let us know about your desired 
+                        journey, and leave the rest to us! We'll make your adventure a 
+                        reality!</p>
                     </div>
                     <div className="sub-with-container">
                         <h1 className="sub-with-title">Destination</h1>
-                        <p className="sub-with-desc">After the planning is complete and full
-                        pre-planned, enjoy your destination!</p>
+                        <p className="sub-with-desc">After the planning is complete and
+                        full pre-planned, enjoy your destination!</p>
                     </div>
                 </div>
             </div>
